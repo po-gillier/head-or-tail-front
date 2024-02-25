062adab7f26ee1244e2f1ee63a315f8019c32f4c
po.gillier <po.gillier.webdev@gmail.com>
2024-02-25 15:13:54 +0100

    init front

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { server } from 'typescript'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    port: 9000
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
